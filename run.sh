4fab2d0c041bba52c58efb1fb9926cdc8a80ea0a
pengtian <pengtianabc@hotmail.com>
2020-07-11 22:37:40 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 HOST_IP=$(ip route get 8.8.8.8 | head -n +1 | tr -s " " | cut -d " " -f 7)
 
-sed -i "s/listen=.*/listen=udp:${HOST_IP}:5060/g" /etc/opensips/opensips.cfg
+sed -i "s/listen=.*/listen=udp:${HOST_IP}:5060 use_workers 8 use_auto_scaling_profile PROFILE_SIP/g" /etc/opensips/opensips.cfg
 
 # skip syslog and run opensips at stderr
-/usr/sbin/opensips -FE
+/usr/sbin/opensips -FE -n 20 -N 20
